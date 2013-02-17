0b17ed2692f05ee27643f4ec1bf20414d52faf5e
aviaviavi <aviaviavi@brick-ME051.(none)>
2013-02-16 16:48:17 -0800

    song added, existing user added

--- echo.py
+++ echo.py
@@ -1,6 +1,7 @@
-from pyechonest import config, catalog, artist
+from pyechonest import config, catalog, artist, song
 from StringIO import StringIO
 import pycurl
+import pickle
 
 config.ECHO_NEST_API_KEY="8IOOXXQIU4NHRY5DY"
 
@@ -17,10 +18,12 @@ config.ECHO_NEST_API_KEY="8IOOXXQIU4NHRY5DY"
 # 	c.perform()
 # 	c.close()
 # 	return storage.getvalue()
+pkl_file = open('user_info.pkl', 'r')
+user_info = pickle.load(pkl_file)
+pkl_file.close()
 
 def create_new_profile(name):
 	c = catalog.Catalog(name, 'general')
-	print 'got it'
 	return c
 
 class User:
@@ -29,8 +32,18 @@ class User:
 		self.cat_id = self.catalog.id
 		self.name = name
 		self.item_id = 0
+		user_info[self.name] = str(self.cat_id)
+		pkl_file = open('user_info.pkl', 'w')
+		pickle.dump(user_info, pkl_file)
+		pkl_file.close()
+
 	def delete(self):
 		self.catalog.delete()
+		del(user_info[self.name])
+		pkl_file = open('user_info.pkl', 'w')
+		pickle.dump(user_info, pkl_file)
+		pkl_file.close()
+
 	def add_artist(self, artist_name):
 		artist_object = artist.Artist(artist_name)
 		item = [{ 'item':
@@ -41,3 +54,24 @@ class User:
 		}]
 		self.catalog.update(item)
 		self.item_id += 1
+
+	def add_song(self, song_name, artist_name):
+		songID = song.search(title=song_name, artist=artist_name)[0].id
+		song_object = song.Song(songID)
+		item = [{'item':
+			{
+				'item_id':str(self.item_id),
+				'song_name': song_object.title
+			}
+		}]
+		self.catalog.update(item)
+		self.item_id+=1
+
+class ExistingUser(User):
+	def __init__(self, name):
+		self.cat_id = user_info[name]
+		self.name = name
+		self.catalog = catalog.Catalog(self.cat_id)
+		self.item_id = len(self.catalog.get_item_dicts())
+
+avi = ExistingUser('avi')
